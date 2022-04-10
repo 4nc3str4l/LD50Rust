83a9fa8d3f9431ae24fac3d2bf5b50116c26775f
4nc3str4l <cmuri_10@hotmail.com>
2022-04-10 17:20:52 +0200

    Trying to render the main scene

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,69 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, scene::InstanceId};
+
 
 fn main() {
     App::new()
+        .insert_resource(Msaa { samples: 4 })
         .add_plugins(DefaultPlugins)
+        .init_resource::<SceneInstance>()
+        .add_startup_system(setup)
+        .add_system(scene_update)
+        .add_system(move_scene_entities)
         .run();
+}
+
+// Resource to hold the scene `instance_id` until it is loaded
+#[derive(Default)]
+struct SceneInstance(Option<InstanceId>);
+
+// Component that will be used to tag entities in the scene
+#[derive(Component)]
+struct EntityInMyScene;
+
+fn setup(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut scene_spawner: ResMut<SceneSpawner>,
+    mut scene_instance: ResMut<SceneInstance>,
+) {
+    commands.spawn_bundle(PerspectiveCameraBundle {
+        transform: Transform::from_xyz(10.0, 10.0, 10.0)
+            .looking_at(Vec3::new(0.0, 0.3, 0.0), Vec3::Y),
+        ..Default::default()
+    });
+
+    // Spawn a second scene, and keep its `instance_id`
+    let instance_id =
+        scene_spawner.spawn(asset_server.load("models/portal.gltf#Scene0"));
+    scene_instance.0 = Some(instance_id);
+}
+
+// This system will wait for the scene to be ready, and then tag entities from
+// the scene with `EntityInMyScene`. All entities from the second scene will be
+// tagged
+fn scene_update(
+    mut commands: Commands,
+    scene_spawner: Res<SceneSpawner>,
+    scene_instance: Res<SceneInstance>,
+    mut done: Local<bool>,
+) {
+    if !*done {
+        if let Some(instance_id) = scene_instance.0 {
+            if let Some(entity_iter) = scene_spawner.iter_instance_entities(instance_id) {
+                entity_iter.for_each(|entity| {
+                    commands.entity(entity).insert(EntityInMyScene);
+                });
+                *done = true;
+            }
+        }
+    }
+}
+
+// This system will move all entities with component `EntityInMyScene`, so all
+// entities from the second scene
+fn move_scene_entities(
+    time: Res<Time>,
+    mut scene_entities: Query<&mut Transform, With<EntityInMyScene>>,
+) {
+
 }
